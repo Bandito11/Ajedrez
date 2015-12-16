838df8bc1158e9a24793df559d274d85f3c306a0
Esteban Morales Morales <red_bandit_11@hotmail.com>
2015-12-16 18:48:29 -0400

    Multidimensional array practice
    This was made as a way to practice using multidimensional arrays.

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] ajedrez =  new string[8,8] {
+            {"T","C","A","R","RE","A","C","T"},
+            {"P","P","P","P","P","P","P","P"},
+            {"0","0","0","0","0","0","0","0"},
+            {"0","0","0","0","0","0","0","0"},
+            {"0","0","0","0","0","0","0","0"},
+            {"0","0","0","0","0","0","0","0"},
+            {"P","P","P","P","P","P","P","P"},
+            {"T","C","A","RE","R","A","C","T"}
+        };
+            //Esto es para mover el peon para el frente
+
+            ajedrez[2, 4] = ajedrez[1, 4];
+            ajedrez[1, 4] = "0";
+
+            for(int i = 0;i < 8; i++)
+            {
+                for (int index = 0; index < 8; index++)
+                {
+                    Console.Write("{0} ", ajedrez[i, index]);
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
